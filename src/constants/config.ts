b694f40c743b4a796a5dc8ccf8b2de2ab11bdfad
nguyenviet3057 <nguyenviet3057@gmail.com>
2023-07-06 20:04:57 +0700

    switch to online host

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {    
-    // BASE_URL: 'https://planx-dev.000webhostapp.com',
-    BASE_URL: 'http://localhost:8000',
+    BASE_URL: 'https://planx-dev.000webhostapp.com',
+    // BASE_URL: 'http://localhost:8000',
     CHECK_AUTH: '/api/login/check',
     LOGIN_FACEBOOK: '/api/login/facebook',
     LOGOUT: '/api/logout',
