958bcae0f6b95ee194874400e3c02106245bfaeb
nguyenviet3057 <nguyenviet3057@gmail.com>
2023-07-16 14:26:58 +0700

    use audio tags instead of module

--- src/components/Chat/ChatContent/Media/AudioMP3.tsx
+++ src/components/Chat/ChatContent/Media/AudioMP3.tsx
@@ -3,15 +3,20 @@ import ReactAudioPlayer from 'react-audio-player';
 
 function AudioMP3(props: any) {
 
-    const { src, autoPlay } = props; 
+    const { src, autoPlay } = props;
 
     return (
         <>
-            <ReactAudioPlayer
+            {/* <audio
                 src={src}
                 autoPlay={autoPlay}
                 controls
-            />
+            /> */}
+            <audio autoPlay={autoPlay} controls>
+                <source src={src} type="audio/ogg" />
+                <source src={src} type="audio/mpeg" />
+                Your browser does not support the audio element.
+            </audio>
         </>
     )
 }
